3131c58aaaa2cb5bed5a447b3b3ee82c7a8c4c20
puppet <88923829@qq.com>
2019-06-15 13:45:21 +0800

    配置文件，zuul搭建

--- ad-gateway/src/main/java/com/ls/adgateway/AdGatewayApplication.java
+++ ad-gateway/src/main/java/com/ls/adgateway/AdGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.ls.adgateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
+@SpringCloudApplication
 public class AdGatewayApplication {
 
     public static void main(String[] args) {
